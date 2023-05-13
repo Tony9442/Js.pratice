c1436f223152ce910fd4334f10422c4f85dcd82d
Geo Tony <geanthony94@gmail.com>
2023-05-13 09:28:20 -0700

    Js pratice

---
+++ main.js
@@ -0,0 +1,36 @@
+let string1 = 'Chidera';
+let string2 = 'Tony';
+let string3 = 'male';
+let string4 = 'Nigeria';
+let string5 = 'Ghana';
+
+let init1 = 20;
+let init2 = 30;
+let init3 = 10;
+let init4 = 5;
+let init5 = 080;
+
+let float1 = 10.1;
+let float2 = 20.2;
+let float3 = 30.3;
+let float4 = 40.4;
+let float5 = 50.5;
+
+let bool1 = true;
+let bool2 = true;
+let bool3 = false;
+let bool4 = false;
+let bool5 = false;
+
+document.write('init1')
+document.write('init2')
+document.write('string3')
+document.write('string4')
+document.write('string5')
+
+document.write('float1')
+document.write('float2')
+document.write('float3')
+document.write('bool4')
+document.write('bool5')
+
