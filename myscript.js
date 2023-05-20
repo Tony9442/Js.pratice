b6ff6c92e524181618237c8aa91ad25db80ad8da
Geo Tony <geanthony94@gmail.com>
2023-05-20 07:29:34 -0700

    #arithmetic.js

---
+++ myscript.js
@@ -0,0 +1,26 @@
+let a = 10;
+let b = 20;
+let c = 30;
+let d = 40;
+let e = 50;
+let f = 60;
+let g = 20.5;
+let h = 30.5;
+let i = 10;
+let j = 3;
+
+
+
+
+
+
+console.log(a + b);
+
+console.log(d - c);
+
+console.log(e * f);
+
+console.log(g / h );
+
+console.log(i % j);
+
